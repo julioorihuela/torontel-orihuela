9248a7ecc655af3b385b43f5874dcaa583260de0
Julio Orihuela <orihuela@gmail.com>
2022-02-16 23:55:31 -0500

    avance desafio5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import NavBar from './components/Navbar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
+
 
 function App() {
   return (
     <div className="App">
       <div>
         <NavBar title='Torontel' />
-      </div>
-            
-      <ItemListContainer greeting="Bienvenid@s!" />
-      <ItemCount />
+      </div>            
+      <ItemListContainer greeting="Bienvenid@s!" />      
     </div>
   );
 }
