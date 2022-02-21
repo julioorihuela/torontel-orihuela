7fa1f71480537dc69beecf80d55edb06ac53a48b
Julio Orihuela <orihuela@gmail.com>
2022-02-21 16:20:39 -0500

    Item Detail

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,9 +1,14 @@
+import './Item.css';
+
 const Item = ({product}) => {
     return (
         <div style={{display: 'flex', flexDirection: 'column', border: '1px solid black', margin: '10px'}}>
           
         <img src={product.img} width="150px" alt={product.name}/> 
         <h2>{product.name}</h2>
+        <h3>Stock: {product.stock}</h3>
+        <h3>Precio: {product.price}</h3>
+
 
         </div>
     );
